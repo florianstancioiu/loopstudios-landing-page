91ff83e53f696923823138e43a4299d5afe4d7c6
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2026-02-28 15:53:23 +0200

    Rename scripts.js to displayCreations.js

--- js/mobileMenu.js
+++ js/mobileMenu.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const $openMenuBtn = document.querySelector(".open-menu-btn");
 const $closeMenuBtn = document.querySelector(".close-menu-btn");
 const $mobileMenu = document.querySelector(".mobile-menu");
